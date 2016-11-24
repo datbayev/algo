08cb94df6d8c115d08a939a7aae1491d8a133e86
Alibek Datbayev <gmegido@gmail.com>
2016-11-24 22:23:47 +0100

    TopCoder SRM 642 Div 2, 500 problem LightSwitchingPuzzle in Java
    Greedy algorithm is used in this implementation. Going from left to the
    right. If we meet turned on light we flip all lights with indexes that
    are divisors of current index.
    Sample JUnit 5 tests from TopCoder are included

--- TopCoder/srm642div2/src/LightSwitchingPuzzle.java
+++ TopCoder/srm642div2/src/LightSwitchingPuzzle.java
@@ -3,26 +3,42 @@ import java.math.*;
 import static java.lang.Math.*;
 
 public class LightSwitchingPuzzle {
-    public byte[] flip(byte[] currentState) {
-        byte[] result = new byte[currentState.length];
-        for (int i = 0; i < currentState.length; i++)
-            result[i] = (byte) (1 - currentState[i]);
+    public int getMinFlips(byte[] a) {
+        int res = 0;
+        int n = a.length;
 
-        return result;
+        for (int i = 1; i < n; i++) {
+            if (a[i] == 1) { // flip divisors ahead
+                res++;
+                int j = i;
+                while (j < n) {
+                    a[j] = (byte)(1 - a[j]);
+                    j += i;
+                }
+            }
+        }
+
+        return res;
     }
 
 	public int minFlips(String str) {
         int n = str.length();
-        byte[] state = new byte[n];
+        byte[] s1 = new byte[n + 1]; // YN -> 10
+        byte[] s2 = new byte[n + 1]; // previous, but reversed
+
         for (int i = 0; i < n; i++)
-            if (str.charAt(i) == 'Y')
-                state[i] = 1;
-        int ind = 0;
-		return 0;
+            if (str.charAt(i) == 'Y') {
+                s1[i + 1] = 1;
+                s2[i + 1] = 0;
+            }
+
+        return getMinFlips(s1);
 	}
 
     public void solve() {
-        String state = "YNYNYNYNYNYNYN";
+        String state = "YYYYYY";
+        int res = minFlips(state);
+        System.out.println(res);
     }
 
     public static void main(String[] args) {
--- TopCoder/srm642div2/test/LightSwitchingPuzzleTest.java
+++ TopCoder/srm642div2/test/LightSwitchingPuzzleTest.java
@@ -1,33 +1,33 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LightSwitchingPuzzleTest {
 	
-	@Test(timeout=2000)
+	@Test()
 	public void test0() {
 		String state = "YYYYYY";
 		assertEquals(1, new LightSwitchingPuzzle().minFlips(state));
 	}
 	
-	@Test(timeout=2000)
+	@Test()
 	public void test1() {
 		String state = "YNYNYNYNY";
 		assertEquals(2, new LightSwitchingPuzzle().minFlips(state));
 	}
 	
-	@Test(timeout=2000)
+	@Test()
 	public void test2() {
 		String state = "NNNNNNNNNN";
 		assertEquals(0, new LightSwitchingPuzzle().minFlips(state));
 	}
 	
-	@Test(timeout=2000)
+	@Test()
 	public void test3() {
 		String state = "YYYNYYYNYYYNYYNYYYYN";
 		assertEquals(4, new LightSwitchingPuzzle().minFlips(state));
 	}
 	
-	@Test(timeout=2000)
+	@Test()
 	public void test4() {
 		String state = "NYNNYNNNYNNNNYNNNNNYNNNNNNYNNNNNNNY";
 		assertEquals(12, new LightSwitchingPuzzle().minFlips(state));
