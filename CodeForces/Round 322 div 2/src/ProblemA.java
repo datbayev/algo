dac2803fbf832e7e96e239cade3cbb23a8b1657b
Alibek Datbayev <gmegido@gmail.com>
2016-11-24 02:42:16 +0100

    Re-organizing CodeForces folder

---
+++ CodeForces/Round 322 div 2/src/ProblemA.java
@@ -0,0 +1,18 @@
+import java.io.PrintWriter;
+import java.util.*;
+
+public class ProblemA {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int red = in.nextInt();
+        int blue = in.nextInt();
+
+        out.print(Math.min(red, blue) + " ");
+        out.print((Math.max(red, blue) - Math.min(red, blue)) / 2);
+
+        out.close();
+    }
+}
---
+++ CodeForces/Round 325 div 2/src/ProblemB.java
@@ -0,0 +1,41 @@
+import java.io.PrintWriter;
+import java.util.*;
+
+public class ProblemB {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        int[] a1 = new int[n];
+        int[] a2 = new int[n];
+        int[] c = new int[n];
+        int minSum = Integer.MAX_VALUE;
+
+        for (int i = 1; i < n; i++) a1[i] = in.nextInt();
+        for (int i = 1; i < n; i++) a2[i] = in.nextInt();
+
+        for (int i = 0; i < n; i++)
+            c[i] = in.nextInt();
+
+        for (int l = 0; l < n; l++) {
+            for (int r = 0; r < n; r++) {
+                if (l != r) {
+                    int res = c[l] + c[r];
+
+                    for (int i = 1; i < l; i++) res += a1[i];
+                    for (int i = l; i < n; i++) res += a2[i];
+
+                    for (int i = r; i < n; i++) res += a1[i];
+                    for (int i = 1; i < r; i++) res += a2[i];
+
+                    minSum = Math.min(minSum, res);
+                }
+            }
+        }
+
+        out.print(minSum);
+        out.close();
+    }
+}
