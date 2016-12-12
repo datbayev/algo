f5120d31be8a1c0fc143732731929b9dbc5020a3
Alibek Datbayev <gmegido@gmail.com>
2016-12-12 23:05:22 +0100

    Added some more clarifications in comments

--- Algorithms and Data Structures/BitManipulation/src/BitOperations.java
+++ Algorithms and Data Structures/BitManipulation/src/BitOperations.java
@@ -7,12 +7,12 @@ public class BitOperations {
 
     // set i-th bit to one
     public static int setBit(int x, int i) {
-        return x | (1 << i);
+        return x | (1 << i); // create 00010000 kind of bit mask to set specific bit
     }
 
     // set i-th bit to zero
     public static int clearBit(int x, int i) {
-        int mask = ~(1 << i);
+        int mask = ~(1 << i); // creates 111101111 kind of bit mask to clear i-th bit
         return x & mask;
     }
 
