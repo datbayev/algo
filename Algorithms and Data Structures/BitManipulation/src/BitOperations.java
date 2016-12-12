bbcff0d97d808e82c9e4b637553814bebf352662
Alibek Datbayev <gmegido@gmail.com>
2016-12-12 23:02:12 +0100

    Bit manipulation: clear bit operation
    Some test are included.

--- Algorithms and Data Structures/BitManipulation/src/BitOperations.java
+++ Algorithms and Data Structures/BitManipulation/src/BitOperations.java
@@ -6,4 +6,12 @@ public class BitOperations {
     public static int setBit(int x, int i) {
         return x | (1 << i);
     }
+
+    // set i-th bit to zero
+    public static int clearBit(int x, int i) {
+        int mask = ~(1 << i);
+        return x & mask;
+    }
+
+
 }
--- Algorithms and Data Structures/BitManipulation/test/BitOperationsTest.java
+++ Algorithms and Data Structures/BitManipulation/test/BitOperationsTest.java
@@ -34,4 +34,27 @@ public class BitOperationsTest {
         assertEquals(20, BitOperations.setBit(x, 4));
         assertEquals(52, BitOperations.setBit(x, 5));
     }
+
+    @Test
+    void testClearBit0() {
+        int x = 20;
+        assertEquals(20, BitOperations.clearBit(x, 0));
+        assertEquals(20, BitOperations.clearBit(x, 1));
+        assertEquals(16, BitOperations.clearBit(x, 2));
+        assertEquals(20, BitOperations.clearBit(x, 3));
+        assertEquals(4, BitOperations.clearBit(x, 4));
+        assertEquals(20, BitOperations.clearBit(x, 5)); // extra zero attached on the left, nothing changes
+    }
+
+    @Test
+    void testClearBit1() {
+        int x = 100;
+        assertEquals(100, BitOperations.clearBit(x, 0));
+        assertEquals(100, BitOperations.clearBit(x, 1));
+        assertEquals(96, BitOperations.clearBit(x, 2));
+        assertEquals(100, BitOperations.clearBit(x, 3));
+        assertEquals(100, BitOperations.clearBit(x, 4));
+        assertEquals(68, BitOperations.clearBit(x, 5));
+        assertEquals(36, BitOperations.clearBit(x, 6));
+    }
 }
