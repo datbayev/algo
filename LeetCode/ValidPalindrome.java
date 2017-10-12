1c30274d711f7984b65c948c2ac8415890a52af9
Alibek Datbayev <gmegido@gmail.com>
2017-10-12 22:41:00 +0200

    Leetcode: 125. Valid Palindrome

---
+++ LeetCode/ValidPalindrome.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/valid-palindrome/description/
+// 125. Valid Palindrome
+
+class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        if (s.length() == 0)
+            return true;
+        
+        s = s.replaceAll("\\W", "").toUpperCase();
+        String reversed = new StringBuilder(s).reverse().toString();
+        
+        return s.equals(reversed);
+    }
+}
