99556641921762f2ac835e2635b3b09a135fdfa6
Alibek Datbayev <gmegido@gmail.com>
2017-11-05 17:22:19 +0100

    Leetcode: 139. Word Break

---
+++ LeetCode/WordBreak.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/word-break/description/
+// 139. Word Break
+
+class WordBreak {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n = s.length();
+        Set<String> set = new HashSet(wordDict);
+        boolean[] dp = new boolean[n + 1];
+        
+        dp[0] = true; // possible for size zero
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                // try to split "s" into two substrings: s[0..j] and s[j..i]
+                if (dp[j] && set.contains(s.substring(j, i))) { 
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+}
