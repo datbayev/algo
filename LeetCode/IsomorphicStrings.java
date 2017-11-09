e4352ab7315ca8a3a60b21c0c871c535545fbfa2
Alibek Datbayev <gmegido@gmail.com>
2017-11-09 23:12:14 +0100

    Leetcode: 205. Isomorphic Strings

---
+++ LeetCode/IsomorphicStrings.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/isomorphic-strings/description/
+// 205. Isomorphic Strings
+
+class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        
+        Map<Character, Character> map = new HashMap();
+        Set<Character> set = new HashSet();
+        
+        for (int i = 0; i < c1.length; i++) {
+            if (!map.containsKey(c1[i])) {
+                if (set.contains(c2[i]))
+                    return false;
+                
+                map.put(c1[i], c2[i]);
+                set.add(c2[i]);
+            } else {
+                if (map.get(c1[i]) != c2[i])
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
