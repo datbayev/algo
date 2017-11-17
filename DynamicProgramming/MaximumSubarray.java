2b22d9b5455f1e4e39c3908ac01fb6771276225b
Alibek Datbayev <gmegido@gmail.com>
2017-11-17 22:52:22 +0100

    Dynamic programming: maximum subarray sum
    Tests included.

---
+++ DynamicProgramming/MaximumSubarray.java
@@ -0,0 +1,20 @@
+package DynamicProgramming;
+
+import java.util.*;
+
+public class MaximumSubarray {
+    public static int getMaxSubArray(List<Integer> list) {
+        int minSum = 0, sum = 0, maxSum = 0;
+        for (int x : list) {
+            sum += x;
+
+            if (minSum > sum)
+                minSum = sum;
+
+            if (maxSum < sum - minSum)
+                maxSum = sum - minSum;
+        }
+
+        return maxSum;
+    }
+}
---
+++ DynamicProgramming/MaximumSubarrayTest.java
@@ -0,0 +1,25 @@
+package DynamicProgramming;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class MaximumSubarrayTest {
+    @Test
+    public void firstTest() {
+        List<Integer> list = Arrays.asList(5, 10, -3, 6, -20, 15, -10, 6, 5, 3);
+        int result = MaximumSubarray.getMaxSubArray(list);
+        assertEquals(19, result);
+    }
+
+    @Test
+    public void secondTest() {
+        List<Integer> list = Arrays.asList(904, 40, 523, 12, -335, -385, -124, 481, -31);
+        int result = MaximumSubarray.getMaxSubArray(list);
+        assertEquals(1479, result);
+    }
+}
