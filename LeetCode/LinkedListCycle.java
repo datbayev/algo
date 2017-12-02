e6f11c4496ba727a9e4c005ec7b2e7a663339e65
Alibek Datbayev <gmegido@gmail.com>
2017-12-02 23:59:04 +0100

    Leetcode: 141. Linked List Cycle

---
+++ LeetCode/LinkedListCycle.java
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/linked-list-cycle/description/
+// 141. Linked List Cycle
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        
+        if (head.next == null)
+            return false;
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (slow != null && fast != null) {
+            slow = slow.next;
+            
+            if (fast.next != null)
+                fast = fast.next.next;
+            else 
+                fast = null;
+            
+            if (slow == fast)
+                return true;
+        }
+        
+        return false;
+    }
+}
