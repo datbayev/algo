ffe6f1c45850e5e4bffc03f570d6fe977a1ce1c8
Alibek Datbayev <gmegido@gmail.com>
2017-12-04 23:22:47 +0100

    Sorting algorithms: rewrite merge sort
    Practice makes perfect.

--- AlgorithmsAndDataStructures/Sorting/MergeSort.java
+++ AlgorithmsAndDataStructures/Sorting/MergeSort.java
@@ -16,17 +16,20 @@ public class MergeSort {
     }
 
     private static void merge(int[] arr, int lo, int mid, int hi) {
-        for (int k = lo; k <= hi; k++)
-            aux[k] = arr[k];
-
         int i = lo;
         int j = mid + 1;
 
-        for (int k = lo; k <= hi; k++) {
-            if (i > mid)              arr[k] = aux[j++]; // if we run out of left side elements
-            else if (j > hi)          arr[k] = aux[i++]; // if we run out of right side elements
-            else if (aux[j] < aux[i]) arr[k] = aux[j++]; // if right element is less
-            else                      arr[k] = aux[i++]; // if left element is less (or equal)
-        }
+        for (int k = lo; k <= hi; k++)
+            aux[k] = arr[k];
+
+        for (int k = lo; k <= hi; k++)
+            if (i > mid)
+                arr[k] = aux[j++];
+            else if (j > hi)
+                arr[k] = aux[i++];
+            else if (aux[j] < aux[i])
+                arr[k] = aux[j++];
+            else
+                arr[k] = aux[i++];
     }
 }
