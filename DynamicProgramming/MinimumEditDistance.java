b5b313b6a625c0ea6ec472f9911498350d69a6a7
Alibek Datbayev <gmegido@gmail.com>
2017-12-08 23:17:34 +0100

    Dynamic Programming: Minimum Edit Distance
    Find minimum edit distance: minimum amount of characters that can be deleted, inserted, or substituted from one string to get the second one.

---
+++ DynamicProgramming/MinimumEditDistance.java
@@ -0,0 +1,24 @@
+package DynamicProgramming;
+
+public class MinimumEditDistance {
+    public static int getDistance(String s1, String s2) {
+        int[][] d = new int[s1.length() + 1][s2.length() + 1];
+
+        for (int i = 1; i <= s1.length(); i++)
+            d[i][0] = i;
+
+        for (int j = 1; j <= s2.length(); j++)
+            d[0][j] = j;
+
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    d[i][j] = d[i - 1][j - 1];
+                else
+                    d[i][j] = 1 + Math.min(d[i - 1][j - 1], Math.min(d[i][j - 1], d[i - 1][j]));
+            }
+        }
+
+        return d[s1.length()][s2.length()];
+    }
+}
---
+++ DynamicProgramming/MinimumEditDistanceTest.java
@@ -0,0 +1,34 @@
+package DynamicProgramming;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MinimumEditDistanceTest {
+    @Test
+    public void test1() {
+        String s1 = "Saturday";
+        String s2 = "Sundays";
+
+        int res = MinimumEditDistance.getDistance(s1, s2);
+        assertEquals(res, 4);
+    }
+
+    @Test
+    public void test2() {
+        String s1 = "Carthorse";
+        String s2 = "Orchestra";
+
+        int res = MinimumEditDistance.getDistance(s1, s2);
+        assertEquals(res, 8);
+    }
+
+    @Test
+    public void test3() {
+        String s1 = "abcdef";
+        String s2 = "azced";
+
+        int res = MinimumEditDistance.getDistance(s1, s2);
+        assertEquals(res, 3);
+    }
+}
