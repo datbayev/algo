63dbb641a8d2aeb315c37aad6899a5f12fa79888
Alibek Datbayev <gmegido@gmail.com>
2017-12-28 00:46:28 +0100

    Leetcode: 742. Closest Leaf in a Binary Tree

---
+++ leetcode/ClosestLeafInABinaryTree.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+// https://leetcode.com/problems/closest-leaf-in-a-binary-tree/description/
+// 742. Closest Leaf in a Binary Tree
+
+import java.util.*;
+
+public class ClosestLeafInABinaryTree {
+    private int[][] g;
+    private boolean[] isLeaf;
+
+    public int findClosestLeaf(TreeNode root, int k) {
+        int res = k;
+        g = new int[1001][1001];
+        boolean[] used = new boolean[1001];
+        isLeaf = new boolean[1001];
+
+        buildGraph(root);
+
+        LinkedList<Integer> q = new LinkedList<>();
+        q.add(k);
+
+        while (!q.isEmpty()) {
+            int cur = q.poll();
+            used[cur] = true;
+
+            if (isLeaf[cur]) {
+                res = cur;
+                break;
+            }
+
+            for (int i = 1; i < 1001; i++) {
+                if (g[i][cur] > 0 && !used[i])
+                    q.add(i);
+            }
+        }
+
+        return res;
+    }
+
+    private void buildGraph(TreeNode root) {
+        if (root.left != null) {
+            g[root.val][root.left.val] = 1;
+            g[root.left.val][root.val] = 1;
+
+            buildGraph(root.left);
+        }
+
+        if (root.right != null) {
+            g[root.val][root.right.val] = 1;
+            g[root.right.val][root.val] = 1;
+
+            buildGraph(root.right);
+        }
+
+        if (root.left == null && root.right == null)
+            isLeaf[root.val] = true;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+}
