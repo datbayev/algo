46627567ca5a53e9292ab26d469b70faf4305b34
Alibek Datbayev <gmegido@gmail.com>
2018-01-14 15:15:32 +0100

    Leetcode: 91. Decode Ways
    Added another solution for the Decode Ways problem.

--- leetcode/DecodeWays.java
+++ leetcode/DecodeWays.java
@@ -49,4 +49,24 @@ class DecodeWays {
         int x = Integer.parseInt(str);
         return x >= 10 && x <= 26;
     }
+
+    public int numDecodings2(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+
+        int n = s.length();
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (s.charAt(i-1) >= '1' && s.charAt(i-1) <= '9')
+                dp[i] = dp[i-1];
+            if (i > 1) {
+                if (s.charAt(i-2) == '1' || s.charAt(i-2) == '2' && s.charAt(i-1) <= '6') {
+                    dp[i] += dp[i-2];
+                }
+            }
+        }
+        return dp[n];
+    }
 }
