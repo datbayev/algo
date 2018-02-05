fd29eb9776b61dcf8ab66f4b3ccf364d29e48e3c
Alibek Datbayev <gmegido@gmail.com>
2018-02-05 19:24:13 +0100

    Simplify BST validation

--- leetcode/ValidateBinarySearchTree.java
+++ leetcode/ValidateBinarySearchTree.java
@@ -5,20 +5,12 @@ package leetcode;
 
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        if (root == null || (root.left == null && root.right == null))
-            return true;
-
         return rec(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    public boolean rec(TreeNode root, long min, long max) {
-        if (root == null)
-            return true;
-
-        if ((long)(root.val) <= min || (long)(root.val) >= max)
-            return false;
-
-        return rec(root.left, min, (long)(root.val)) && rec(root.right, (long)(root.val), max);
+    public boolean rec(TreeNode node, long min, long max) {
+        return node == null ||
+                node.val < max && node.val > min && rec(node.left, node.val, min) && rec(node.right, max, node.val);
     }
 
     public class TreeNode {
