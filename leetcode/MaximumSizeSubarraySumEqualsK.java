5095b6bee4a0b3d629c7fbc817b240096b5918e8
Alibek Datbayev <gmegido@gmail.com>
2018-02-10 15:54:45 +0100

    Added brute-force solution to Leetcode 325 problem.

--- leetcode/MaximumSizeSubarraySumEqualsK.java
+++ leetcode/MaximumSizeSubarraySumEqualsK.java
@@ -26,4 +26,31 @@ public class MaximumSizeSubarraySumEqualsK {
 
         return max;
     }
+
+    public int maxSubArrayLenBruteForce(int[] nums, int k) {
+        int n = nums.length;
+        if (n == 0)
+            return 0;
+
+        int max = Integer.MIN_VALUE;
+
+        int[] sums = new int[n];
+        sums[0] = nums[0];
+        if (sums[0] == k)
+            max = 1;
+
+        for (int i = 1; i < n; i++) {
+            sums[i] = sums[i - 1] + nums[i];
+            if (sums[i] == k)
+                max = i + 1;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++)
+                if (sums[j] - sums[i] == k)
+                    max = Math.max(max, j - i);
+        }
+
+        return max == Integer.MIN_VALUE ? 0 : max;
+    }
 }
