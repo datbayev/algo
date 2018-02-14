cbd6128aa6349f5674818ca19c8850fa5dfa9e0b
Alibek Datbayev <gmegido@gmail.com>
2018-02-14 23:44:49 +0100

    Simplified Leetcode 215. Kth Largest Element in an Array
    Removed redundant overlapping logic.

--- leetcode/KthLargestElementInAnArray.java
+++ leetcode/KthLargestElementInAnArray.java
@@ -7,18 +7,14 @@ import java.util.*;
 
 class KthLargestElementInAnArray {
     public int findKthLargest(int[] nums, int k) {
-        Queue<Integer> pq = new PriorityQueue(nums.length, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer a, Integer b) {
-                return b - a;
-            }
-        });
-        
-        for (int i = 0; i < nums.length; i++)
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k + 1);
+
+        for (int i = 0; i < nums.length; i++) {
             pq.add(nums[i]);
-        
-        for (int i = 1; i < k; i++)
-            pq.poll();
+
+            if (i >= k)
+                pq.poll();
+        }
         
         return pq.peek();
     }
