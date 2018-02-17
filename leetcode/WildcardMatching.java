60b359ffff6a79f7a3d8bd0830f6bc0b5b66f59d
Alibek Datbayev <gmegido@gmail.com>
2018-02-17 22:32:36 +0100

    Leetcode: 44. Wildcard Matching
    Added DP solution.

--- leetcode/WildcardMatching.java
+++ leetcode/WildcardMatching.java
@@ -7,6 +7,31 @@ import java.util.*;
 
 public class WildcardMatching {
     public boolean isMatch(String s, String p) {
+        boolean[][] d = new boolean[s.length() + 1][p.length() + 1];
+
+        d[s.length()][p.length()] = true;
+
+        for (int i = p.length() - 1; i >= 0; i--)
+            if (p.charAt(i) != '*')
+                break;
+            else
+                d[s.length()][i] = true;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+                if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')
+                    d[i][j] = d[i + 1][j + 1];
+                else if (p.charAt(j) == '*')
+                    d[i][j] = d[i + 1][j] || d[i][j + 1];
+                else
+                    d[i][j] = false;
+            }
+        }
+
+        return d[0][0];
+    }
+
+    public boolean isMatch_BFS(String s, String p) {
         if (p.equals(""))
             return s.equals("");
 
