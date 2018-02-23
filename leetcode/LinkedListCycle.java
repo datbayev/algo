fe952bd1171f119e7aa0b3ea57baebd5efc62420
Alibek Datbayev <gmegido@gmail.com>
2018-02-23 16:26:03 +0100

    Simplified linked list cycle detection solution

--- leetcode/LinkedListCycle.java
+++ leetcode/LinkedListCycle.java
@@ -14,27 +14,20 @@ public class LinkedListCycle {
     }
 
     public boolean hasCycle(ListNode head) {
-        if (head == null)
+        if (head == null || head.next == null)
             return false;
-        
-        if (head.next == null)
-            return false;
-        
+
         ListNode slow = head;
         ListNode fast = head;
-        
-        while (slow != null && fast != null) {
+
+        while (fast != null && fast.next != null) {
             slow = slow.next;
-            
-            if (fast.next != null)
-                fast = fast.next.next;
-            else 
-                fast = null;
-            
+            fast = fast.next.next;
+
             if (slow == fast)
                 return true;
         }
-        
+
         return false;
     }
 }
