b99dde81cd89edaa0074512cd84f778bcad2bb65
Alibek Datbayev <gmegido@gmail.com>
2018-02-23 21:25:10 +0100

    Optimized leetcode 81. Search in Rotated Sorted Array II

--- leetcode/SearchInRotatedSortedArrayII.java
+++ leetcode/SearchInRotatedSortedArrayII.java
@@ -5,54 +5,44 @@ package leetcode;
 
 class SearchInRotatedSortedArrayII {
     public boolean search(int[] nums, int target) {
-        int n = nums.length;
-        
-        if (n == 0)
+        if (nums.length == 0)
             return false;
-        
-        if (n == 1)
-            return target == nums[0];
-        
-        if (nums[0] < nums[n - 1])
-            return binSearch(nums, target, 0, n - 1) != -1;
-        
-        int[] nums2 = new int[2 * n + 2];
-        for (int i = 0; i < n; i++)
-            nums2[i] = nums[i];
-        
-        int p1 = 0;
-        int p2 = n - 1;
-        
-        while (p1 < n - 1 && nums[p1] >= nums2[p2]) {
-            p2++;
-            nums2[p2] = nums[p1];
-            p1++;
-        }
-        
-        return binSearch(nums2, target, p1, p2) != -1;
+
+        int left = 0;
+        while (left < nums.length - 1 && nums[left] == nums[nums.length - 1])
+            left++;
+
+        int right = nums.length - 1;
+        while (right > 0 && nums[right] == nums[right - 1])
+            right--;
+
+        return binSearch(nums, target, left, right);
     }
-    
-    public int binSearch(int[] nums, int target, int start, int end) {
-        int left = start;
-        int right = end;
-        int minIndex = -1;
-        
+
+    private boolean binSearch(int[] nums, int target, int left, int right) {
         while (left < right) {
             int mid = left + (right - left) / 2;
-            
-            if (nums[mid] < target)
-                left = mid+ 1;
-            else if (nums[mid] > target)
-                right = mid;
-            else {
-                minIndex = Math.min(minIndex, mid);
-                right = mid;
+
+            if (nums[mid] == target)
+                return true;
+
+            // we need to determine which side is sorted
+            // "left -> mid" or "mid -> right"
+            if (nums[left] <= nums[mid]) { // left to mid is sorted
+                if (target <= nums[mid] && target >= nums[left]) { // lies in left sorted side
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            } else { // right to mid is sorted
+                if (target >= nums[mid] && target <= nums[right]) { // list in right sorted side
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
             }
         }
-        
-        if (nums[left] == target)
-            return left;
-        
-        return minIndex;
+
+        return nums[left] == target;
     }
 }
