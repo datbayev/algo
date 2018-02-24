efeea76ae5064f15320b21380dad3b584a2ecb9e
Alibek Datbayev <gmegido@gmail.com>
2018-02-24 13:10:07 +0100

    Optimized solution for Count and say problem

--- leetcode/CountAndSay.java
+++ leetcode/CountAndSay.java
@@ -7,23 +7,24 @@ class CountAndSay {
     public String countAndSay(int n) {
         if (n == 1)
             return "1";
-        
-        String prev = countAndSay(n - 1);
-        
-        int i = 0;
-        String res = "";
-        while (i < prev.length()) {
-            int count = 1;
-            int num = prev.charAt(i) - '0';
-            while (i < prev.length() - 1 && prev.charAt(i) == prev.charAt(i + 1)) {
+
+        char[] prev = countAndSay(n - 1).toCharArray();
+        StringBuilder res = new StringBuilder("");
+
+        char c = prev[0];
+        int count = 1;
+
+        for (int i = 1; i < prev.length; i++) {
+            if (prev[i] == c)
                 count++;
-                i++;
+            else {
+                res.append(count).append(c);
+                count = 1;
+                c = prev[i];
             }
-            
-            i++;
-            res = String.format("%s%d%d", res, count, num);
         }
-        
-        return res;
+
+        res.append(count).append(c);
+        return res.toString();
     }
 }
