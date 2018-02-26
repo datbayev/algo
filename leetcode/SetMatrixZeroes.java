fc3369ccd0ded926ca5777296378f5f994ee0bdb
Alibek Datbayev <gmegido@gmail.com>
2018-02-26 11:33:04 +0100

    Added better solution for leetcode 73. Set Matrix Zeroes
    This approach uses constant extra space, in previous it was O(N+M).

--- leetcode/SetMatrixZeroes.java
+++ leetcode/SetMatrixZeroes.java
@@ -6,7 +6,59 @@ package leetcode;
 import java.util.*;
 
 public class SetMatrixZeroes {
+
+    // Constant extra space
     public void setZeroes(int[][] matrix) {
+        if (matrix.length == 0)
+            return;
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        for (int i = 0; i < n; i++)
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+
+        for (int j = 0; j < m; j++)
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+
+        for (int i = 1; i < n; i++)
+            for (int j = 1; j < m; j++)
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+
+        for (int i = 1; i < n; i++)
+            if (matrix[i][0] == 0)
+                for (int j = 0; j < m; j++)
+                    matrix[i][j] = 0;
+
+        for (int j = 1; j < m; j++)
+            if (matrix[0][j] == 0)
+                for (int i = 0; i < n; i++)
+                    matrix[i][j] = 0;
+
+        if (firstColZero)
+            for (int i = 0; i < n; i++)
+                matrix[i][0] = 0;
+
+        if (firstRowZero)
+            for (int j = 0; j < m; j++)
+                matrix[0][j] = 0;
+    }
+
+    // this approach is better than storing extra matrix
+    // but still uses O(N+M) memory
+    public void setZeroesWithExtraMemory(int[][] matrix) {
         if (matrix.length == 0)
             return;
 
