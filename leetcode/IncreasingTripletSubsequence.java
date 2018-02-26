821a1c86f4d314fa23aa3fd4f012b5127acf18d3
Alibek Datbayev <gmegido@gmail.com>
2018-02-26 15:42:33 +0100

    Leetcode: 334. Increasing Triplet Subsequence
    Keep tracking of smallest and seconds smallest numbers.

---
+++ leetcode/IncreasingTripletSubsequence.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+// https://leetcode.com/problems/increasing-triplet-subsequence/
+// 334. Increasing Triplet Subsequence
+
+public class IncreasingTripletSubsequence {
+    public boolean increasingTriplet(int[] nums) {
+        if (nums.length < 3)
+            return false;
+
+        int min0 = nums[0];
+        int min1 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (min0 >= num) {
+                min0 = num;
+            } else if (min1 >= num)
+                min1 = num;
+            else
+                return true;
+        }
+
+        return false;
+    }
+}
