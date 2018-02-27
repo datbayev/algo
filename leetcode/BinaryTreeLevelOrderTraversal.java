bbfdaf2d6da19cd12f1dba19e33be946cb5ccc42
Alibek Datbayev <gmegido@gmail.com>
2018-02-27 19:08:47 +0100

    Added non-recursive solution for "level order binary tree" problem

--- leetcode/BinaryTreeLevelOrderTraversal.java
+++ leetcode/BinaryTreeLevelOrderTraversal.java
@@ -33,4 +33,39 @@ class BinaryTreeLevelOrderTraversal {
         traverse(node.left, level + 1, res);
         traverse(node.right, level + 1, res);
     }
+
+    public List<List<Integer>> levelOrderQueues(TreeNode root) {
+        if (root == null)
+            return new ArrayList<>();
+
+        Queue<TreeNode> q = new LinkedList<>();
+
+        q.add(root);
+        q.add(null);
+
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+
+        while (!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            if (cur == null) {
+                if (q.isEmpty())
+                    break;
+
+                res.add(new ArrayList<>());
+                q.add(null);
+                continue;
+            }
+
+            res.get(res.size() - 1).add(cur.val);
+
+            if (cur.left != null)
+                q.add(cur.left);
+
+            if (cur.right != null)
+                q.add(cur.right);
+        }
+
+        return res;
+    }
 }
