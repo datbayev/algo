e6b5b23d048c8a69bfdce228801f5623f5338a06
Alibek Datbayev <gmegido@gmail.com>
2018-02-28 11:31:12 +0100

    Simplified and optimized leetcode 388. Longest Absolute File Path

--- leetcode/LongestAbsoluteFilePath.java
+++ leetcode/LongestAbsoluteFilePath.java
@@ -3,71 +3,22 @@ package leetcode;
 // https://leetcode.com/problems/longest-absolute-file-path
 // 388. Longest Absolute File Path
 
-import java.util.*;
-
 class LongestAbsoluteFilePath {
-    int maxLen;
-    Map<Integer, Folder> levels;
-    
     public int lengthLongestPath(String input) {
-        maxLen = 0;
-        levels = new HashMap();
-        
-        processFilesStructure(input);
-        
-        return maxLen;
-    }
-    
-    public void processFilesStructure(String input) {
+        int max = 0;
+
         String[] lines = input.split("\n");
-        
+        int[] stack = new int[lines.length + 1]; // stack[i] - length of i-th line
         for (String line : lines) {
-            String name = line.replaceAll("\t", "");
-            int curDepthLevel = (line.length() - name.length());
-            
-            if (isFile(line)) {
-                int curPathLen = name.length() + curDepthLevel; // adding slashes length
-                
-                if (curDepthLevel > 0)
-                    curPathLen += levels.get(curDepthLevel - 1).getPathLen();
-                
-                if (maxLen < curPathLen)
-                    maxLen = curPathLen;
-                
-            } else {
-                Folder newFolder = new Folder(name);
-                levels.put(curDepthLevel, newFolder);
-                
-                if (curDepthLevel > 0)
-                    newFolder.parent = levels.get(curDepthLevel - 1);    
-            }
+            int level = line.lastIndexOf("\t") + 1; // how many times "tabbed"
+            int cur = stack[level] + line.length() - level + 1;
+
+            stack[level + 1] = cur;
+
+            if (line.contains("."))
+                max = Math.max(max, cur - 1);
         }
+
+        return max;
     }
-    
-    public boolean isFile(String line) {
-        return line.indexOf('.') >= 0;
-    }
-}
-
-class Folder {
-    Folder parent;
-    String name;
-
-    public Folder(String name) {
-        this.name = name;
-    }
-
-    public int getPathLen() {
-        if (Cache.storage.containsKey(this.hashCode()))
-            return Cache.storage.get(this.hashCode());
-
-        int pathLen = this.name.length() + (this.parent != null ? this.parent.getPathLen() : 0);
-        Cache.storage.put(this.hashCode(), pathLen);
-
-        return pathLen;
-    }
-}
-
-class Cache {
-    public static Map<Integer, Integer> storage = new HashMap();
 }
