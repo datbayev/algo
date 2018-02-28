a5cfca85bb55fcf6b4b3a57ded2424d794deabd8
Alibek Datbayev <gmegido@gmail.com>
2018-02-28 13:40:15 +0100

    Removed redundant extra check

--- leetcode/PalindromeLinkedListFast.java
+++ leetcode/PalindromeLinkedListFast.java
@@ -47,7 +47,7 @@ class PalindromeLinkedListFast {
         ListNode slow = head;
         ListNode fast = head;
         
-        while (slow.next != null && fast.next != null && fast.next.next != null) {
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
